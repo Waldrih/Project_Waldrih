6da088ed7ea9ecf1333719ca1cd0ee8f9675abef
Robert Duda <waldrih_wow3@interia.pl>
2022-03-26 16:04:20 +0100

    Add scene instance

--- Testy/2/Main_2.py
+++ Testy/2/Main_2.py
@@ -9,10 +9,18 @@ HEIGHT = 360
 WIDTH = 690
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 screen.fill(GRAY)
-#scene_1_path = str(r'C:\Users\waldr\Desktop\Project_Waldrih\Testy\2\scene_1.png')
 
-scene_1 = Scene()
-screen.blit(scene_1.scene_1,(0,0))
+img_1 = pygame.image.load('button_1.png')
+img_2 = pygame.image.load('button_2.png')
+img_scene_1 = pygame.image.load('scene_1.png')
+img_scene_2 = pygame.image.load('scene_2.png')
+img_scene_3 = pygame.image.load('scene_3.png')
+
+scene_1 = Scene(img_scene_1)
+scene_2 = Scene(img_scene_2)
+scene_3 = Scene(img_scene_3)
+
+scene_1.draw()
 pygame.display.update()
 while True:
 
--- Testy/2/Scene.py
+++ Testy/2/Scene.py
@@ -1,8 +1,14 @@
 import pygame.image
 from pygame import Surface
 
+
 class Scene:
-    def __init__(self):
-        self.scene_1 = pygame.image.load(r'C:\Users\waldr\Desktop\Project_Waldrih\Testy\2\scene_1.png')
-        self.scene_2 = pygame.image.load(r'C:\Users\waldr\Desktop\Project_Waldrih\Testy\2\scene_2.png')
-        self.scene_3 = pygame.image.load(r'C:\Users\waldr\Desktop\Project_Waldrih\Testy\2\scene_3.png')
+    def __init__(self,image: Surface):
+        self.HEIGHT = 360
+        self.WIDTH = 690
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+
+        self.image = image
+
+    def draw(self):
+        self.screen.blit(self.image,(0,0))
