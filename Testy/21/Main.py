2ce8502efd57d5ec050afd7029b532b62361cedf
Hellrot <waldrih_wow3@interia.pl>
2022-04-01 12:35:37 +0200

    Trying to change direction with class Direction

--- Testy/21/Main.py
+++ Testy/21/Main.py
@@ -10,7 +10,14 @@ WIDTH = 690
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Button demo')
 screen.fill(GRAY)
-time_to_blit = None
+current_position = None
+
+class Direction:
+    NORTH = "NORTH"
+    EAST = "EAST"
+    SOUTH = "SOUTH"
+    WEST = "WEST"
+
 
 class Board:
     main_img = None
@@ -26,6 +33,9 @@ class Board:
 
     def get_main_img(self):
         return self.main_img
+
+    def get_description_img(self):
+        return self.description_img
 
 class Position:
     def __init__(self, x: int, y: int, board: Board ):
@@ -104,48 +114,56 @@ position_1_0 = Position(1,0,board_2)
 position_2_0 = Position(2,0,board_3)
 position_2_1 = Position(2,1,board_4)
 
-positions = [position_1_1,position_1_0,position_2_0]
-
+positions = [position_1_1,position_1_0,position_2_0,position_2_1]
+direction = "lol"
 def choise_button(N: bool, E: bool, S: bool, W: bool):
     if N and button_N.draw():
         print("Możesz iść na połnoc")
-        bool_n = button_N.draw()
+        print(f"Kierunek {Direction.NORTH}")
+        return Direction.NORTH
     elif N == False and button_N.draw():
         print("Nie możesz iść na północ")
 
     if E and button_E.draw():
         print("Możesz iść na wschód")
-        bool_e = button_E.draw()
+        print(f"Kierunek {Direction.EAST}")
+        return Direction.EAST
     elif E == False and button_E.draw():
         print("Nie możesz iść na wschód")
 
     if S and button_S.draw():
         print("Możesz iść na południe")
-        bool_s = button_S.draw()
+        print(f"Kierunek {Direction.SOUTH}")
+        return Direction.SOUTH
     elif S == False and button_S.draw():
         print("Nie możesz iść na południe")
 
     if W and button_W.draw():
         print("Możesz iść na zachód")
-        bool_w = button_W.draw()
+        print(f"Kierunek {Direction.WEST}")
+        return Direction.WEST
     elif W == False and button_W.draw():
         print("Nie możesz iść na zachód")
 
 def draw_board(x_start: int, y_start: int):
+    if direction == "NORTH":
+        y_start += 1
+        print(f"y_start= {y_start}")
+
     for position in positions:
         if position.get_x() == x_start and position.get_y() == y_start:
             screen.blit(position.board.get_main_img(),(0,0))
-        if button_des.draw():
-            screen.blit(img_description_1,(45,30))
-
-            pygame.display.update()
-            time.sleep(3)
-            print("opis")
-
-
-    choise_button(True,False,False,False)
+            if button_des.draw():
+                screen.blit(position.board.get_description_img(), (45, 30))
+                pygame.display.update()
+                time.sleep(3)
+                print("opis")
+            direction ==choise_button(position.board.N, position.board.E, position.board.S, position.board.W)
 
 
+
+start_pos_x = 1
+start_pos_y = 1
 while True:
     draw_board(1,1)
     pygame.display.update()
